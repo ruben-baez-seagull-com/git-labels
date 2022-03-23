2c12f5f044d4d32649cf6f4dc2d75085d0428ef2
Ruben <ruben.baez@mojix.com>
2022-03-22 21:35:02 -0500

    add commit sha entry

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,21 @@ import {
 
 async function run() {
     const token = getInput("github-token", { required: true });
+    const sha = getInput("commit-sha", { required: true });
     const client = new GitHub(token);
 
-    const labelNames = await getLabels(client);
+    const labelNames = await getLabels(client, sha);
 
     setOutput("result", labelNames);
 }
 
 async function getLabels(
-    client: GitHub
+    client: GitHub,
+    sha: string
 ): Promise<string[]> {
     const owner = context.repo.owner;
     const repo = context.repo.repo;
-    const commit_sha = context.sha;
+    const commit_sha = sha;
 
     const response = await client.repos.listPullRequestsAssociatedWithCommit({
         owner,
